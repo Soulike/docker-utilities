a7a547837752968b7a97eb4bc7f09941afa28880
Soulike <git_anonymous@foxmail.com>
2024-08-04 15:04:23 +0800

    fix(vultr-ddns): fix DDNS update is not fired upon start

--- apps/vultr-ddns/src/index.ts
+++ apps/vultr-ddns/src/index.ts
@@ -13,29 +13,33 @@ const TTL = Number.parseInt(env['DDNS_INTERVAL'] ?? '600');
 assert.ok(DOMAIN, 'Environment variable DOMAIN is not set properly.');
 assert.ok(
   IPV4_SUBDOMAIN || IPV6_SUBDOMAIN,
-  'Both environment variable IPV4_SUBDOMAIN and IPV6_SUBDOMAIN are not set properly.',
+  'Both environment variable IPV4_SUBDOMAIN and IPV6_SUBDOMAIN are not set properly.'
 );
 
 const API_KEY = VultrAPI.getAPIKey();
 
 if (IPV4_SUBDOMAIN) {
   const dnsConfigurator = new DNSConfigurator(API_KEY, DOMAIN, IPV4_SUBDOMAIN);
-  setInterval(async () => {
+  const update = async () => {
     const address = await IPQuery.getIPv4();
     await dnsConfigurator.setIPv4(address, TTL);
     Logger.info(`DNS A record of ${IPV4_SUBDOMAIN}.${DOMAIN} is updated to ${address}.`);
-  }, TTL * 1000);
+  };
+  update();
+  setInterval(update, TTL * 1000);
 }
 
 if (IPV6_SUBDOMAIN) {
-  setInterval(async () => {
-    const dnsConfigurator = new DNSConfigurator(
-      API_KEY,
-      DOMAIN,
-      IPV6_SUBDOMAIN,
-    );
+  const dnsConfigurator = new DNSConfigurator(
+    API_KEY,
+    DOMAIN,
+    IPV6_SUBDOMAIN
+  );
+  const update = async () => {
     const address = await IPQuery.getIPv6();
     await dnsConfigurator.setIPv6(address, TTL);
     Logger.info(`DNS AAAA record of ${IPV6_SUBDOMAIN}.${DOMAIN} is updated to ${address}.`);
-  }, TTL * 1000);
+  };
+  update();
+  setInterval(update, TTL * 1000);
 }
