e373680f4dfa50927482b4ced12dd3dab24bc183
Soulike <git_anonymous@foxmail.com>
2024-09-19 07:11:20 +0800

    fix: fix test IPv4 error

--- apps/vultr-ddns/src/IPQuery.ts
+++ apps/vultr-ddns/src/IPQuery.ts
@@ -8,19 +8,19 @@ export class IPQuery {
   }
 
   static async getIPv4() {
-    const ipv4 = await IPQuery.getIPFromRemote('https://ipv4.icanhazip.com');
+    const ipv4 = await IPQuery.getIPFromRemote('https://api.ipify.org');
     assert.ok(
       Address4.isValid(ipv4),
-      'Remote returns an invalid IPv4 address.',
+      `Remote returns an invalid IPv4 address: ${ipv4}.`,
     );
     return ipv4;
   }
 
   static async getIPv6() {
-    const ipv6 = await IPQuery.getIPFromRemote('https://ipv6.icanhazip.com');
+    const ipv6 = await IPQuery.getIPFromRemote('https://api6.ipify.org');
     assert.ok(
       Address6.isValid(ipv6),
-      'Remote returns an invalid IPv4 address.',
+      `Remote returns an invalid IPv6 address: ${ipv6}.`,
     );
     return ipv6;
   }
